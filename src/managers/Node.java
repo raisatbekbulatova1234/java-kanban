1525616d32bf4785d3cbb1b300dea107105d41f1
Raisat <raisat.bekbulatova1234@gmail.ru>
2025-06-09 22:58:14 +0300

    rewrite class Node for CheckStyle error again

--- src/managers/Node.java
+++ src/managers/Node.java
@@ -10,7 +10,6 @@ public class Node<Task> {
         this.next = null;
         this.prev = null;
     }
-<<<<<<< HEAD
     /// я не могу найти, все переписала, не проходит, а время идет, помогите пожалуйста, сил уже нет на это все
     /*Error:  /home/runner/work/java-kanban/java-kanban/./src/managers/Node.java:3:19: Class type name 'Task' must match pattern '(^[A-Z][0-9]?)$|([A-Z][a-zA-Z0-9]*[T]$)'. [ClassTypeParameterName]
     Audit done.
@@ -18,8 +17,6 @@ public class Node<Task> {
             Error: Process completed with exit code 1.
 
      */
+}
 
-}
-=======
-}
->>>>>>> 9f72765bda81b6fc18a484c96467a5583aa23972
+
